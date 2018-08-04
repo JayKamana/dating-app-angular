851a6612665bcd5bd8d9a72111b620495ae9eadb
James Kamana <jaykamana@gmail.com>
2018-08-04 15:53:19 +0300

    Add login auth service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
 import { NavComponent } from './nav/nav.component';
+import { AuthService } from './_services/auth.service';
 
 @NgModule({
   declarations: [AppComponent, ValueComponent, NavComponent],
   imports: [BrowserModule, HttpModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, AuthService]
 })
 export class AppModule {}
